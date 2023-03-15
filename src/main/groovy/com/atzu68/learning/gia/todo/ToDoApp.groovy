232f27ae7152d5881cd418b22870d0e6468be1f7
Champion <gongguochuanqi@gmail.com>
2023-03-15 17:34:41 +0800

    ...

--- src/main/groovy/com/atzu68/learning/gia/todo/ToDoApp.groovy
+++ src/main/groovy/com/atzu68/learning/gia/todo/ToDoApp.groovy
@@ -2,11 +2,14 @@ package com.atzu68.learning.gia.todo
 
 import com.atzu68.learning.gia.todo.utils.CommandLineInput
 import com.atzu68.learning.gia.todo.utils.CommandLineInputHandler
+import org.apache.commons.lang3.CharUtils
 
 import static com.atzu68.learning.gia.todo.utils.CommandLineInput.EXIT
 import static com.atzu68.learning.gia.todo.utils.CommandLineInput.getCommandLineInputForInput
 
 class ToDoApp {
+
+    static final char DEFAULT_INPUT = '\u0000' as char
 
     static void main(String[] args) {
         def commandLineInputHandler = new CommandLineInputHandler()
@@ -15,6 +18,7 @@ class ToDoApp {
         while (command != EXIT) {
             commandLineInputHandler.printOptions()
             def input = commandLineInputHandler.readInput()
+            input = CharUtils.toChar(input, DEFAULT_INPUT).toString()
             command = getCommandLineInputForInput(input)
             commandLineInputHandler.processInput(command)
         }
