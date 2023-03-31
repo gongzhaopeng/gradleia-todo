0a061e071c89e32d41de081c7aa90a7845be4a5e
Champion <gongguochuanqi@gmail.com>
2023-03-31 18:03:11 +0800

    ...

---
+++ cloudbees-plugin/object-plugin/buildSrc/src/main/groovy/com/atzu68/learning/gia/plugins/cloudbees/CloudBeesPlugin.groovy
@@ -0,0 +1,11 @@
+package com.atzu68.learning.gia.plugins.cloudbees
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class CloudBeesPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        // TODO
+    }
+}
